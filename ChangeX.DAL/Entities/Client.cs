7b109932dd925c0f73a5924a1f2b011734b491ba
mahmoud zahran <mmzahran30@gmail.com>
2026-08-17 15:06:40 +0300

    add project entity

--- ChangeX.DAL/Entities/Client.cs
+++ ChangeX.DAL/Entities/Client.cs
@@ -8,6 +8,8 @@ namespace ChangeX.DAL.Entities
         public Guid ID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string? Description { get; set; }
+        public string? Address { get; set; }
         public string ContactInfo { get; set; }
 
     }

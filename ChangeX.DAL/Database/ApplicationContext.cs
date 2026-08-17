24fbd6a150afa8055031787af05851378c5aee23
mahmoud zahran <mmzahran30@gmail.com>
2026-08-17 11:30:23 +0300

    update gitignore

--- ChangeX.DAL/Database/ApplicationContext.cs
+++ ChangeX.DAL/Database/ApplicationContext.cs
@@ -12,15 +12,5 @@ namespace ChangeX.DAL.Database
         public DbSet<Invoice> Invoices { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<User> Users { get; set; }
-
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)  To Cancel Delete 
-        //{
-        //    foreach (var relationship in modelBuilder.Model.GetEntityTypes()
-        //        .SelectMany(e => e.GetForeignKeys()))
-        //    {
-        //        relationship.DeleteBehavior = DeleteBehavior.NoAction;
-        //    }
-        //}
     }
 }

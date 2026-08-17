9cc46161b3870f8e21e43b8412a7260b78ff2cf6
Mazen <mazen.mohamed52004@gmail.com>
2026-08-17 13:57:29 +0300

    add user

--- ChangeX.DAL/Entities/Client.cs
+++ ChangeX.DAL/Entities/Client.cs
@@ -5,10 +5,10 @@ namespace ChangeX.DAL.Entities
 {
     public class Client
     {
-        [Key]
         public Guid ID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string ContactInfo { get; set; }
+
     }
 }
--- ChangeX.DAL/Entities/Detail.cs
+++ ChangeX.DAL/Entities/Detail.cs
@@ -5,5 +5,20 @@ namespace ChangeX.DAL.Entities
 {
     public class Detail
     {
+        [Key]
+        public Guid ID { get; set; }
+
+        [ForeignKey(nameof(CR))]
+        public Guid CRID { get; set; }
+
+        public CR CR { get; set; } = null!;
+
+        public string Attachment { get; set; } = string.Empty;
+
+        public string Comment { get; set; } = string.Empty;
+
+        public string State { get; set; } = string.Empty;
+
+        public DateTime UploadedTime { get; set; }
     }
 }

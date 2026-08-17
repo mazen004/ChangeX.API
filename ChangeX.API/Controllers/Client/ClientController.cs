861b2fd0d2a71d5a3ac19e3c9f4e983c9e24c0a1
mahmoud zahran <mmzahran30@gmail.com>
2026-08-17 14:15:15 +0300

    add client CRUD apis

---
+++ ChangeX.API/Controllers/Client/ClientController.cs
@@ -0,0 +1,43 @@
+﻿using ChangeX.DAL.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ChangeX.BLL.DTOs;
+using ChangeX.DAL.Entities;
+
+
+namespace ChangeX.API.Controllers.Client
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ApplicationContext dbcontext;
+
+        public ClientController(ApplicationContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        public IActionResult GetClients()
+        {
+            // Logic to retrieve clients from the database
+            return Ok(new { message = "Get all clients", data= dbcontext.Clients.ToList()});
+        }
+
+        [HttpPost]
+        public IActionResult CreateClient(ClientDto clientDto)
+        {
+            // Logic to create a new client in the database
+            var client = new ()
+            {
+                Name = clientDto.Name,
+                Email = clientDto.Email,
+                ContactInfo = clientDto.ContactInfo
+            };
+            dbcontext.Clients.Add(client);
+            dbcontext.SaveChanges();
+            return Ok(new { message = "Client created successfully", data = client });
+        }
+    }
+}
---
+++ ChangeX.BLL/DTOs/ClientDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeX.BLL.DTOs
+{
+    public class ClientDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ContactInfo { get; set; }
+    }
+}

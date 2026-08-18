4eedc6ebb0eb8cc9fca55d36d12f58ded46a0ea1
Mazen <mazen.mohamed52004@gmail.com>
2026-08-18 10:47:52 +0300

    add User Service To Controller

--- ChangeX.API/Program.cs
+++ ChangeX.API/Program.cs
@@ -1,6 +1,7 @@
-
+using ChangeX.BLL.Services;
 using ChangeX.DAL.Database;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 namespace ChangeX.API
 {
     public class Program
@@ -14,6 +15,12 @@ namespace ChangeX.API
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddTransient<IUserServices, UserServices>();
+
+            //builder.Services.AddAutoMapper(typeof(Program));
+
+            builder.Services.AddCors();
             builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
 

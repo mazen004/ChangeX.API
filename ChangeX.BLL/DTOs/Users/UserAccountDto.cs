0527de0b1c941028d44c95f576994b7b6f6f3c3f
Mazen <mazen.mohamed52004@gmail.com>
2026-08-18 14:49:26 +0300

    Add Get and Post Request on User

--- ChangeX.BLL/DTOs/Users/UserAccountDto.cs
+++ ChangeX.BLL/DTOs/Users/UserAccountDto.cs
@@ -10,7 +10,7 @@ namespace ChangeX.BLL.DTOs.Users
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string SystemRole { get; set; } = string.Empty;
-        public Guid ClientID { get; set; }
+        public Guid ClientID { get; set; } = Guid.Empty;
         public string ClientName { get; set; } = string.Empty;
     }
 }

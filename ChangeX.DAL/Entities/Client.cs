c9670975c4e6f72e04f397249d0e01a4e0d04643
body-sawan <your-email@example.com>
2026-08-18 11:02:08 +0300

    ..

--- ChangeX.DAL/Entities/Client.cs
+++ ChangeX.DAL/Entities/Client.cs
@@ -5,7 +5,7 @@ namespace ChangeX.DAL.Entities
 {
     public class Client
     {
-        public Guid ID { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string? Description { get; set; }

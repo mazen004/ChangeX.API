f58ddb535a966309c691816735ad8779bae96039
habibamohamedsafey55-del <habibamohamedsafey55@gmail.com>
2026-08-18 12:40:38 +0300

    ..

---
+++ ChangeX.API/Controllers/Admin/ProjectsAdminController.cs
@@ -0,0 +1,127 @@
+﻿using ChangeX.BLL.DTOs;
+using ChangeX.BLL.DTOs.Users;
+using ChangeX.DAL.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChangeX.API.Controllers.Admin
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    public class ProjectAdminController : ControllerBase
+    {
+        private readonly ApplicationContext _dbcontext;
+
+        public ProjectAdminController(ApplicationContext dbcontext)
+        {
+            this._dbcontext = dbcontext;
+        }
+
+        // GET: api/Project
+        [HttpGet]
+        public IActionResult GetProjects()
+        {
+            return Ok(new
+            {
+                message = "Get all projects",
+                data = _dbcontext.Projects.ToList()
+            });
+        }
+
+        // GET: api/Project/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetProjectById(Guid id)
+        {
+            var project = _dbcontext.Projects.Find(id);
+
+            if (project == null)
+            {
+                return NotFound(new
+                {
+                    message = "Project not found"
+                });
+            }
+
+            return Ok(new
+            {
+                message = "Project found",
+                data = project
+            });
+        }
+
+        // POST: api/Project
+        [HttpPost]
+        public IActionResult CreateProject(ProjectDto projectDto)
+        {
+            var project = new DAL.Entities.Project()
+            {
+                Name = projectDto.Name,
+                Description = projectDto.Description,
+                Scope = projectDto.Scope,
+                ClientID = projectDto.ClientID,
+                State = projectDto.State
+            };
+
+            _dbcontext.Projects.Add(project);
+            _dbcontext.SaveChanges();
+
+            return Ok(new
+            {
+                message = "Project created successfully",
+                data = project
+            });
+        }
+
+        // PUT: api/Project/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateProject(Guid id, ProjectDto projectDto)
+        {
+            var project = _dbcontext.Projects.Find(id);
+
+            if (project == null)
+            {
+                return NotFound(new
+                {
+                    message = "Project not found"
+                });
+            }
+
+            project.Name = projectDto.Name;
+            project.Description = projectDto.Description;
+            project.Scope = projectDto.Scope;
+            project.ClientID = projectDto.ClientID;
+            project.State = projectDto.State;
+
+            _dbcontext.SaveChanges();
+
+            return Ok(new
+            {
+                message = "Project updated successfully",
+                data = project
+            });
+        }
+
+        // DELETE: api/Project/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProject(Guid id)
+        {
+            var project = _dbcontext.Projects.Find(id);
+
+            if (project == null)
+            {
+                return NotFound(new
+                {
+                    message = "Project not found"
+                });
+            }
+
+            _dbcontext.Projects.Remove(project);
+            _dbcontext.SaveChanges();
+
+            return Ok(new
+            {
+                message = "Project deleted successfully"
+            });
+        }
+    }
+}
---
+++ ChangeX.API/Controllers/Client/ProjectsClientController.cs
@@ -0,0 +1,126 @@
+﻿using ChangeX.DAL.Database;
+using ChangeX.BLL.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using ChangeX.BLL.DTOs.Users;
+
+namespace ChangeX.API.Controllers.Client
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectClientController : ControllerBase
+    {
+        private readonly ApplicationContext dbcontext;
+
+        public ProjectClientController(ApplicationContext _dbcontext)
+        {
+            this.dbcontext = _dbcontext;
+        }
+
+        // GET: api/Project
+        [HttpGet]
+        public IActionResult GetProjects()
+        {
+            return Ok(new
+            {
+                message = "Get all projects",
+                data = dbcontext.Projects.ToList()
+            });
+        }
+
+        // GET: api/Project/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetProjectById(Guid id)
+        {
+            var project = dbcontext.Projects.Find(id);
+
+            if (project == null)
+            {
+                return NotFound(new
+                {
+                    message = "Project not found"
+                });
+            }
+
+            return Ok(new
+            {
+                message = "Project found",
+                data = project
+            });
+        }
+
+        // POST: api/Project
+        [HttpPost]
+        public IActionResult CreateProject(ProjectDto projectDto)
+        {
+            var project = new DAL.Entities.Project()
+            {
+                Name = projectDto.Name,
+                Description = projectDto.Description,
+                Scope = projectDto.Scope,
+                ClientID = projectDto.ClientID,
+                State = projectDto.State
+            };
+
+            dbcontext.Projects.Add(project);
+            dbcontext.SaveChanges();
+
+            return Ok(new
+            {
+                message = "Project created successfully",
+                data = project
+            });
+        }
+
+        // PUT: api/Project/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateProject(Guid id, ProjectDto projectDto)
+        {
+            var project = dbcontext.Projects.Find(id);
+
+            if (project == null)
+            {
+                return NotFound(new
+                {
+                    message = "Project not found"
+                });
+            }
+
+            project.Name = projectDto.Name;
+            project.Description = projectDto.Description;
+            project.Scope = projectDto.Scope;
+            project.ClientID = projectDto.ClientID;
+            project.State = projectDto.State;
+
+            dbcontext.SaveChanges();
+
+            return Ok(new
+            {
+                message = "Project updated successfully",
+                data = project
+            });
+        }
+
+        // DELETE: api/Project/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProject(Guid id)
+        {
+            var project = dbcontext.Projects.Find(id);
+
+            if (project == null)
+            {
+                return NotFound(new
+                {
+                    message = "Project not found"
+                });
+            }
+
+            dbcontext.Projects.Remove(project);
+            dbcontext.SaveChanges();
+
+            return Ok(new
+            {
+                message = "Project deleted successfully"
+            });
+        }
+    }
+}

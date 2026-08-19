d129fefdb2e5e69b3bca06fcb22f956e0174cc83
youssefkarim <youssefkarim.fathy@gmail.com>
2026-08-19 15:40:50 +0300

    add IStatus

---
+++ ChangeX.BLL/Interfaces/IstatusService.cs
@@ -0,0 +1,14 @@
+﻿using ChangeX.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeX.BLL.Interfaces
+{
+    internal interface IstatusService
+    {
+        Task<CRStatus> GetCurrentStatus(Guid ID);
+        Task<CRStatus> GetAvailableStatus(Guid ID);
+        Task<CRStatus> ChangeStatus(CRStatus status);
+    }
+}
---
+++ ChangeX.BLL/Services/StatusService.cs
@@ -0,0 +1,42 @@
+﻿using ChangeX.BLL.Interfaces;
+using ChangeX.DAL.Database;
+using ChangeX.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeX.BLL.Services
+{
+    public class StatusService
+    {
+        private readonly ApplicationContext dbcontext;
+
+        public StatusService(ApplicationContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        
+        public async Task<CRStatus> GetCurrentStatus(Guid ID)
+        {
+            var Status = await dbcontext.CRStatues
+                .Where(c => c.ID == ID)
+                .FirstOrDefaultAsync();
+            if (Status == null)
+                throw new Exception($"Status not found.");
+            return Status;
+        }
+        public async Task<string[]> GetAvailableStatus(Guid ID)
+        {
+            var Status = await dbcontext.CRStatues
+               .Where(c => c.ID == ID)
+               .FirstOrDefaultAsync();
+            return Status.AvailableStatusIDs.Split(",");
+        }
+        //public async Task<CRStatus> ChangeStatus(CRStatus status)
+        //{
+
+        //}
+    }
+}

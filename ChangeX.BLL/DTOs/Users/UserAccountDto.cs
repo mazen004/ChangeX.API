e8364856e5167d7bac97ede613ddfe0d924cb2c9
Mazen <mazen.mohamed52004@gmail.com>
2026-08-20 13:15:01 +0300

    Fix User Controller

--- ChangeX.BLL/DTOs/Users/UserAccountDto.cs
+++ ChangeX.BLL/DTOs/Users/UserAccountDto.cs
@@ -10,8 +10,7 @@ namespace ChangeX.BLL.DTOs.Users
         public Guid ID { get; set; } = Guid.Empty;
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string SystemRole { get; set; } = string.Empty;
-        public bool IsPrimaryContact { get; set; } = false;
+        public bool SystemRole { get; set; } = false;
         public Guid ClientID { get; set; } = Guid.Empty;
         public string ClientName { get; set; } = string.Empty;
     }
